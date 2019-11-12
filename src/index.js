cf5bd4de8dedfd9bca0ec42153253666e02e9468
Tata <tats.dev@gmail.com>
2019-11-13 02:40:00 +0300

    main page skeleton

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./store";
 import App from "./components/app";
+import ErrorBoundry from "./components/error-boundry/error-boundry";
+import BookstoreService from "./services/bookstore-service";
+import {BookstoreServiceProvider} from "./components/bookstore-service-context";
 
-ReactDOM.render(<App />,
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ErrorBoundry>
+            <BookstoreServiceProvider value={bookstoreService}>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </BookstoreServiceProvider>
+        </ErrorBoundry>
+    </Provider>,
     document.getElementById('root'));
