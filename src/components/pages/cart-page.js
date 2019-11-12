82b13e16f2c5ff97b1faa80a2141181008ea0896
Tata <tats.dev@gmail.com>
2019-11-13 02:39:36 +0300

    dummy pages created

---
+++ src/components/pages/cart-page.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const CartPage = () => {
+    return(
+        <h2>Cart Page</h2>
+    );
+};
+export default CartPage;
---
+++ src/components/pages/home-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookList from "../book-list";
+
+const HomePage = () => {
+
+    return(
+        <div>
+            <h2>Home Page</h2>
+            <BookList/>
+        </div>
+    );
+};
+export default HomePage;
