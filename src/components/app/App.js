e0236023bd221d7e2d447bc8d50ce3b643224fbf
Tata <tats.dev@gmail.com>
2019-11-12 20:47:52 +0300

    project skeleton

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Spinner from "../spinner";
+
+export default class App extends Component {
+
+    render(){
+        return(
+            <div>
+                <div>hello</div>
+                <Spinner/>
+            </div>
+        );
+    }
+}
+
---
+++ src/components/error-boundry/error-boundry.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import ErrorIndicator from "../error-indicator";
+
+export default class ErrorBoundry extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({hasError: true});
+    }
+
+
+    render() {
+        return this.state.hasError ? <ErrorIndicator/> : this.props.children;
+    };
+};
