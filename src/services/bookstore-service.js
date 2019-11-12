43f185105d89844468f40740797ef4b8b2cba193
Tata <tats.dev@gmail.com>
2019-11-13 02:38:29 +0300

    test data added

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,7 +1,18 @@
 export default class BookstoreService {
 
     getBooks() {
-        return [];
+        return [
+            {
+                id: 1,
+                title: 'Jenkins 2: Up and Running',
+                author: 'Brent Laster'
+            },
+            {
+                id: 2,
+                title: 'The Site Reliability Workbook',
+                author: 'Niall Murphy, David Rensin, Betsy Beyer'
+            }
+        ];
     }
 
 }
