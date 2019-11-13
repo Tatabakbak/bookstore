10d30e43585a6b1693f3b1ddf36613fa489d766d
Tata <tats.dev@gmail.com>
2019-11-14 00:11:45 +0300

    dummy timeout

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,22 +1,28 @@
 export default class BookstoreService {
 
+    data = [
+        {
+            id: 1,
+            title: 'Jenkins 2: Up and Running',
+            author: 'Brent Laster',
+            price: 32,
+            coverImage: '/dummy-data/img/cover_1.jpg'
+        },
+        {
+            id: 2,
+            title: 'The Site Reliability Workbook',
+            author: 'Niall Murphy, David Rensin, Betsy Beyer',
+            price: 45,
+            coverImage: '/dummy-data/img/cover_2.jpg'
+        }
+    ];
+
     getBooks() {
-        return [
-            {
-                id: 1,
-                title: 'Jenkins 2: Up and Running',
-                author: 'Brent Laster',
-                price: 32,
-                coverImage: '/dummy-data/img/cover_1.jpg'
-            },
-            {
-                id: 2,
-                title: 'The Site Reliability Workbook',
-                author: 'Niall Murphy, David Rensin, Betsy Beyer',
-                price: 45,
-                coverImage: '/dummy-data/img/cover_2.jpg'
-            }
-        ];
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(this.data)
+            }, 700);
+        });
     }
 
 }
